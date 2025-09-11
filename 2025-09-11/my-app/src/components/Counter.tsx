3366956b0eb358b9cd93d65e80f14e69a09892c6
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-11 23:27:35 +0300

    feat(counter): add buttons for +100, +50, +25, +1 and negatives

---
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import "../App.css"
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  function increaseCounter(amount: number) {
+    setCount(count => count + amount)
+  }
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p>Count: {count}</p>
+      <div className="buttons">
+        <button onClick={() => increaseCounter(100)}>+100</button>
+        <button onClick={() => increaseCounter(50)}>+50</button>
+        <button onClick={() => increaseCounter(25)}>+25</button>
+        <button onClick={() => increaseCounter(1)}>+1</button>
+        <button onClick={() => increaseCounter(-1)}>-1</button>
+        <button onClick={() => increaseCounter(-25)}>-25</button>
+        <button onClick={() => increaseCounter(-50)}>-50</button>
+        <button onClick={() => increaseCounter(-100)}>-100</button>
+      </div>
+    </>
+  )
+}
+
+export default Counter
