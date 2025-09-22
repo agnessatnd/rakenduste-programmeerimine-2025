c5f896fb8d4cd543ddedd0d00196dfefe910092a
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-22 21:54:44 +0300

    feat(about): add About page layout and lorem ipsum content

--- 2025-09-18/my-app/src/pages/About.tsx
+++ 2025-09-18/my-app/src/pages/About.tsx
@@ -1,5 +1,45 @@
-import { Typography } from "@mui/material"
+import { Box, Stack, Typography, Divider } from "@mui/material"
 
 export default function About() {
-  return <Typography variant="h4">About</Typography>
+  return (
+    <Box>
+      <Typography
+        variant="h4"
+        gutterBottom
+        align="center"
+      >
+        About
+      </Typography>
+
+      <Divider sx={{ mb: 3 }} />
+
+      <Stack spacing={2}>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at
+          nulla et sapien tincidunt fermentum. Phasellus dignissim urna at odio
+          tempor, in iaculis sem sagittis.
+        </Typography>
+
+        <Typography>
+          Donec posuere, sapien in suscipit placerat, nisl ex lobortis eros, non
+          posuere lorem justo vel nulla. Integer imperdiet, sem at tincidunt
+          ullamcorper, velit mi porttitor lorem, sit amet feugiat felis neque at
+          tortor.
+        </Typography>
+
+        <Typography>
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Sed vitae sapien quis neque tincidunt
+          tristique. Phasellus sed finibus tellus. Morbi viverra bibendum
+          turpis, et tincidunt justo.
+        </Typography>
+
+        <Typography>
+          In hac habitasse platea dictumst. Curabitur vel justo eget mauris
+          faucibus euismod. Nunc sed malesuada nisl. Curabitur in sapien nec
+          arcu pulvinar suscipit.
+        </Typography>
+      </Stack>
+    </Box>
+  )
 }
