d485065867a3823b715facb6027db86388a8258a
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-22 21:15:42 +0300

    feat(pages): add Home page with search field and Accordions using MUI

--- 2025-09-18/my-app/src/main.tsx
+++ 2025-09-18/my-app/src/main.tsx
@@ -2,18 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { createHashRouter, RouterProvider } from "react-router-dom"
 import "./index.css"
-import App from "./App"
 
 import Something from "./pages/Something"
 import About from "./pages/About"
 import RootLayout from "./components/RootLayout"
+import Home from "./pages/Home"
 
 const router = createHashRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <App /> },
+      { index: true, element: <Home /> },
       { path: "about", element: <About /> },
       { path: "something", element: <Something /> },
     ],
---
+++ 2025-09-18/my-app/src/pages/Home.tsx
@@ -0,0 +1,83 @@
+import {
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  Button,
+  Card,
+  CardContent,
+} from "@mui/material"
+import Accordion from "@mui/material/Accordion"
+import AccordionActions from "@mui/material/AccordionActions"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import { useState } from "react"
+
+export default function Home() {
+  const [query, setQuery] = useState("")
+  return (
+    <Box>
+      <Card>
+        <CardContent>
+          <Stack spacing={2}>
+            <TextField
+              label="Otsi midagi"
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+              placeholder="sisesta tekst..."
+              fullWidth
+            />
+            <Button variant="contained">Otsi</Button>
+          </Stack>
+        </CardContent>
+      </Card>
+      <br />
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <Typography component="span">Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+          >
+            <Typography component="span">Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
+        <Accordion defaultExpanded>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3-content"
+            id="panel3-header"
+          >
+            <Typography component="span">Accordion Actions</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+          <AccordionActions>
+            <Button>Cancel</Button>
+            <Button>Agree</Button>
+          </AccordionActions>
+        </Accordion>
+      </div>
+    </Box>
+  )
+}
