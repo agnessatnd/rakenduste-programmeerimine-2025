b1b763224d564db6a3174bb9d96cbdee47c412f6
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-27 22:33:48 +0300

    feat(cats-backend): add CRUD routes with validators

--- 2025-09-25/agnessa/backend/routes/cats.routes.js
+++ 2025-09-25/agnessa/backend/routes/cats.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const { body, param } = require("express-validator");
 const router = express.Router();
+
 const catsController = require("../controllers/cats.controller");
 const {
   catsRouteMiddleware,
@@ -8,10 +10,21 @@ const {
 
 router.use(catsRouteMiddleware);
 
-// /cats/ Get endpoint level middleware
 router.get("/", catsGetRouteMiddleware, catsController.read);
-router.post("/", catsController.create);
-router.put("/", catsController.update);
-router.delete("/", catsController.delete);
+
+router.post(
+  "/",
+  body("name").isString().trim().isLength({ min: 1, max: 50 }),
+  catsController.create
+);
+
+router.put(
+  "/:id",
+  param("id").isUUID(),
+  body("name").optional().isString().trim().isLength({ min: 1, max: 50 }),
+  catsController.update
+);
+
+router.delete("/:id", param("id").isUUID(), catsController.delete);
 
 module.exports = router;
