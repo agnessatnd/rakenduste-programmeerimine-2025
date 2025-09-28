048e11048e22a83da5411d23f06edae18548e4ba
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-28 23:56:18 +0300

    feat(api): mount /todos route and add global error handler

--- 2025-09-25/todo/backend/index.js
+++ 2025-09-25/todo/backend/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+const catsRoutes = require("./routes/todos.routes");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/todos", catsRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Server error" });
 });
 
 app.listen(port, () => {
