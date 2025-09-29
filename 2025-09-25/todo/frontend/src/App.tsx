fd1136ad5db28a6bd7585fe1a6b03672ce29032d
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-29 23:33:22 +0300

    feat(todo-frontend): add simple page switch between Todos and AdminTodos

--- 2025-09-25/todo/frontend/src/App.tsx
+++ 2025-09-25/todo/frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import './App.css'
-import Todos from './components/Todos'
+import { useState } from "react"
+import "./App.css"
+import Todos from "./components/Todos"
+import AdminTodos from "./components/AdminTodos"
 
 function App() {
-  return <Todos />;
+  const [page, setPage] = useState<"todos" | "admin">("todos")
+
+  return (
+    <div>
+      <nav>
+        <button onClick={() => setPage("todos")}>Todos</button>
+        <button onClick={() => setPage("admin")}>Admin Todos</button>
+      </nav>
+
+      {page === "todos" && <Todos />}
+      {page === "admin" && <AdminTodos />}
+      <br />
+    </div>
+  )
 }
 
 export default App
