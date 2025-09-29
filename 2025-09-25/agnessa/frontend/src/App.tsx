51083ad500d3a247d437aac997b36f44a5fffc65
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-29 17:58:16 +0300

    feat(examples): add PropDrilling and Context components

--- 2025-09-25/agnessa/frontend/src/App.tsx
+++ 2025-09-25/agnessa/frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
 import Cats from './components/Cats'
+import PropDrilling from './examples/PropDrilling'
+import Context from './examples/Context'
 
 function App() {
-
-  return <Cats />;
+  return (
+    <>
+      <Cats />
+      <Context />
+      <PropDrilling />
+    </>
+  );
 }
 
 export default App
