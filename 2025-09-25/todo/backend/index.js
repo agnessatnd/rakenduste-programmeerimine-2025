4c5a59db6ed23926a5c9d472193115b0f38ee713
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-30 00:30:09 +0300

    chore(todo-backend): load JWT secret from .env and mount /auth routes

--- 2025-09-25/todo/backend/index.js
+++ 2025-09-25/todo/backend/index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
 const catsRoutes = require("./routes/todos.routes");
 const adminRoutes = require("./routes/admin.routes");
+const authRoutes = require("./routes/auth.routes");
 
 app.use(cors());
 
@@ -11,6 +14,7 @@ app.use(express.json());
 
 app.use("/todos", catsRoutes);
 app.use("/admin", adminRoutes);
+app.use("/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err);
