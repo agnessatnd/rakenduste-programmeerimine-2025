b83ade56cc68cfee854312f6f549d28e24fcd5e5
Agnessa Tund <agnessa.tund@tptlive.ee>
2025-09-29 23:27:47 +0300

    feat(todo-backend): add admin routes to express app

--- 2025-09-25/todo/backend/index.js
+++ 2025-09-25/todo/backend/index.js
@@ -3,12 +3,14 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 const catsRoutes = require("./routes/todos.routes");
+const adminRoutes = require("./routes/admin.routes");
 
 app.use(cors());
 
 app.use(express.json());
 
 app.use("/todos", catsRoutes);
+app.use("/admin", adminRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err);
